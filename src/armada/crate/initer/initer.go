94b15623b412f35ac3b0635a8e371fc2f94997ae
Matt Hartzler <matt@cryptopanic.org>
2015-03-23 16:32:45 -0500

    handle multi connections better and be more secure on socket file

--- src/armada/crate/initer/initer.go
+++ src/armada/crate/initer/initer.go
@@ -1,5 +1,5 @@
-// small process that is made to run inside a crate container and control
-// container processes.
+// small process that is made to run inside a crate container as PID 1
+// and control container processes.
 package initer
 
 import (
@@ -9,6 +9,7 @@ import (
 	"net"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 var SocketFile = "crate.socket"
@@ -17,48 +18,47 @@ func CrateInit() {
 
 	// clear if exists
 	os.Remove("/" + SocketFile)
+
+	// set permission on listen to 0600...
+	oldmask := syscall.Umask(0177)
 	l, err := net.Listen("unix", "/"+SocketFile)
 	if err != nil {
 		panic(err)
 	}
 	defer l.Close()
 
-	fmt.Println("CRATE-INIT: hello from init!")
+	// restore
+	syscall.Umask(oldmask)
 
+	fmt.Println("CRATE-INIT: ready...")
 	for {
 		conn, err := l.Accept()
 		if err != nil {
 			fatal(err)
-			continue
+			continue // try again...
 		}
-		var buf [1024]byte
-		n, err := conn.Read(buf[:])
-		if err != nil {
-			fatal(err)
-			continue
-		}
-		fmt.Printf("CRATE-INIT: %s\n", string(buf[:n]))
-
-		var args crate.RunArgs
-		if err = json.Unmarshal(buf[:n], &args); err != nil {
-			fatal(err)
-			continue
-		}
-		fmt.Printf("CRATE-INIT: DECODED: %s\n", args)
-
-		cmd := exec.Command(args.Args[0], args.Args[1:]...)
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		cmd.Stdin = os.Stdin
-
-		if err := cmd.Run(); err != nil {
-			fatal(err)
-			continue
-		}
-		//fmt.Println("CRATE-INIT: [RUN]: ", string(bytes))
-
-		conn.Close()
+		go func() {
+			defer conn.Close()
+			if err := handle(conn); err != nil {
+				fatal(err)
+			}
+		}()
 	}
+}
+
+func handle(conn net.Conn) error {
+	var args crate.RunArgs
+	if err := json.NewDecoder(conn).Decode(&args); err != nil {
+		return err
+	}
+	fmt.Printf("CRATE-INIT: DECODED: %s\n", args)
+
+	// just inherit out/err for now...
+	cmd := exec.Command(args.Args[0], args.Args[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	return cmd.Run()
 }
 
 func fatal(err error) {
